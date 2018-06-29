7928f02159a31a667a935978ef653efa9fb93809
Neil Saitug <niftynei@gmail.com>
2018-06-29 01:11:17 -0700

    finish twenty one, entab

--- ch-1/twenty-one.c
+++ ch-1/twenty-one.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 #define LINEMAX 1000
-#define TABCOUNT 10
+#define TABCOUNT 8
 
 int fetchline(char s[], int lim);
 
@@ -12,32 +12,26 @@ int main() {
 		if (len == 1) continue;
 		int tabcounter = 0;
 		int tabIndex = 0;
+		int space_count = 0;
 		for (int i = 0; i <= len; i++) {
 			char c = line[i];
 			if (c == ' ') {
-				int pointerAt = i + (TABCOUNT - tabcounter - 2);
-				int isTabs = pointerAt < len;
-				for (int p = pointerAt; p > i && p < len; p--) {
-					if (line[p] != ' ') {
-						isTabs = 0;
-						break;
-					}
-				}
-				if (isTabs) {
-					entabbed[tabIndex++] = '\t';
-					// advance i to the end of the tabstop
-					i += TABCOUNT - tabcounter - 3;
-					tabcounter = 0;
-				} else {
-					entabbed[tabIndex++] = c;
-					++tabcounter;
-				}
+				++space_count;
+				++tabcounter;
 			} else {
+				while (space_count > 0) {
+					entabbed[tabIndex++] = ' ';
+					space_count--;
+				}
 				entabbed[tabIndex++] = c;
 				++tabcounter;
 			}
 			if (tabcounter == TABCOUNT) {
 				tabcounter = 0;
+				if (space_count > 0) {
+					entabbed[tabIndex++] = '\t';
+					space_count = 0;
+				}
 			}
 		}
 		printf("%s", entabbed);
